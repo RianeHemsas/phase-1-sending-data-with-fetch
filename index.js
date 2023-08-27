79323d26c92c8ffa28be410b285d446d869b3f10
Hemsas Riane <rayan.hemsas@gmail.com>
2023-08-27 12:42:56 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+
+
+    function submitData(name, email) {
+   
+      return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email
+        })
+      })
+      .then(response=>  response.json() )
+      .then(data =>{
+        const newElement = document.createElement("p");
+        newElement.textContent=data.id;
+        document.body.appendChild(newElement);
+      })
+      .catch(error => {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = `${error.message}`;
+        document.body.appendChild(errorMessage);
+      });
+        
+        }
